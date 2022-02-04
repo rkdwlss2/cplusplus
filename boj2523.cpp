7ecd63e62db40351378e97778e4e5c15073ff540
rkdwlss2 <rkdwlss2@gmail.com>
2022-02-04 22:56:32 +0900

    boj2522

--- boj2523.cpp
+++ boj2523.cpp
@@ -1,26 +1,26 @@
+////
+////  boj2523.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/02/03.
+////
 //
-//  boj2523.cpp
-//  cplusplus
+//#include <iostream>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/02/03.
-//
-
-#include <iostream>
-using namespace std;
-
-int main(){
-    int n;
-    cin>>n;
-    for (int i = 1 ; i <n;i++){
-        for (int j = 0 ; j <i;j++){
-            cout<<'*';
-        }
-        cout<<'\n';
-    }
-    for (int i=n;i>=1;i--){
-        for (int j = 0 ; j <i;j++){
-            cout<<'*';
-        }
-        cout<<'\n';
-    }
-}
+//int main(){
+//    int n;
+//    cin>>n;
+//    for (int i = 1 ; i <n;i++){
+//        for (int j = 0 ; j <i;j++){
+//            cout<<'*';
+//        }
+//        cout<<'\n';
+//    }
+//    for (int i=n;i>=1;i--){
+//        for (int j = 0 ; j <i;j++){
+//            cout<<'*';
+//        }
+//        cout<<'\n';
+//    }
+//}
