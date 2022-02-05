fdc73018de184d82c318388acff5cdb1d0c4f25b
rkdwlss2 <rkdwlss2@gmail.com>
2022-02-05 22:27:20 +0900

    boj1600

---
+++ boj2522.cpp
@@ -0,0 +1,37 @@
+////
+////  boj2523.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/02/03.
+////
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin>>n;
+//    for (int i = 1 ; i <n;i++){
+//        int tmp=n-i;
+//        while(tmp--){
+//            cout<<' ';
+//        }
+//        
+//        for (int j = 0 ; j <i;j++){
+//            cout<<'*';
+//        }
+//        cout<<'\n';
+//    }
+//    for (int i=n;i>=1;i--){
+//        int tmp=n-
+//        i;
+//        while(tmp--){
+//            cout<<' ';
+//        }
+//        for (int j = 0 ; j <i;j++){
+//            cout<<'*';
+//        }
+//        cout<<'\n';
+//    }
+//}
+//
