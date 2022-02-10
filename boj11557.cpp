da07b0998f9acea58d90cb732aa8060d642682b8
rkdwlss2 <rkdwlss2@gmail.com>
2022-02-10 22:17:13 +0900

    boj8370

--- boj11557.cpp
+++ boj11557.cpp
@@ -1,31 +1,31 @@
+////
+////  boj11557.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/02/09.
+////
 //
-//  boj11557.cpp
-//  cplusplus
+//#include <iostream>
+//#include <string>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/02/09.
-//
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main(){
-    int tc;
-    cin>>tc;
-    while (tc--) {
-        int n;
-        cin>>n;
-        int Big=-1;
-        string name ="";
-        for (int i = 0; i < n;i++){
-            int a;
-            string b;
-            cin>>b>>a;
-            if (a>Big){
-                Big=a;
-                name=b;
-            }
-        }
-        cout<<name<<'\n';
-    }
-}
+//int main(){
+//    int tc;
+//    cin>>tc;
+//    while (tc--) {
+//        int n;
+//        cin>>n;
+//        int Big=-1;
+//        string name ="";
+//        for (int i = 0; i < n;i++){
+//            int a;
+//            string b;
+//            cin>>b>>a;
+//            if (a>Big){
+//                Big=a;
+//                name=b;
+//            }
+//        }
+//        cout<<name<<'\n';
+//    }
+//}
---
+++ boj8370.cpp
@@ -0,0 +1,15 @@
+//
+//  boj8370.cpp
+//  cplusplus
+//
+//  Created by 강명진 on 2022/02/10.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,k,n1,k1;
+    cin>>n>>k>>n1>>k1;
+    cout<<n*k+n1*k1<<'\n';
+}
