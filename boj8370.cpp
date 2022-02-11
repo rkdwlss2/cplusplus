8eed95618feeef3e06de523a703e80071ba00d27
rkdwlss2 <rkdwlss2@gmail.com>
2022-02-11 21:22:15 +0900

    boj10170

--- boj8370.cpp
+++ boj8370.cpp
@@ -1,15 +1,15 @@
+////
+////  boj8370.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/02/10.
+////
 //
-//  boj8370.cpp
-//  cplusplus
+//#include <iostream>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/02/10.
-//
-
-#include <iostream>
-using namespace std;
-
-int main(){
-    int n,k,n1,k1;
-    cin>>n>>k>>n1>>k1;
-    cout<<n*k+n1*k1<<'\n';
-}
+//int main(){
+//    int n,k,n1,k1;
+//    cin>>n>>k>>n1>>k1;
+//    cout<<n*k+n1*k1<<'\n';
+//}
