dd29d9ad1b3d5a242e7e8ddc3aa1eecf21b14902
rkdwlss2 <rkdwlss2@gmail.com>
2022-02-12 22:05:05 +0900

    boj10991

---
+++ boj10991.cpp
@@ -0,0 +1,26 @@
+//
+//  boj10991.cpp
+//  cplusplus
+//
+//  Created by 강명진 on 2022/02/12.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    for (int i = 1 ; i <=n;i++){
+        int now = n-i;
+        for (int j = 0 ; j <now ;j++){
+            cout<<' ';
+        }
+        for (int j = 0 ; j <i;j++){
+            cout<<"* ";
+        }
+        cout<<'\n';
+    }
+}
