1ac176544cf4a1b2521cbe28336a13ed5ffee5a8
rkdwlss2 <rkdwlss2@gmail.com>
2022-02-27 18:05:16 +0900

    boj2011

--- boj10173.cpp
+++ boj10173.cpp
@@ -1,33 +1,33 @@
+////
+////  boj10173.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/02/26.
+////
 //
-//  boj10173.cpp
-//  cplusplus
+//#include <iostream>
+//#include <string>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/02/26.
-//
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    while(true){
-        string tmp;
-        getline(cin,tmp);
-        if (tmp.compare("EOI")==0){
-            break;
-        }
-        bool ok=false;
-        for (int i = 0 ; i < tmp.size()-4+1;i++){
-            if ((tmp[i]=='n'||tmp[i]=='N')&&(tmp[i+1]=='e'||tmp[i+1]=='E')&&(tmp[i+2]=='m'||tmp[i+2]=='M')&&(tmp[i+3]=='o'||tmp[i+3]=='O')){
-                cout<<"Found"<<'\n';
-                ok=true;
-                break;
-            }
-        }
-        if (!ok){
-            cout<<"Missing"<<'\n';
-        }
-    }
-}
+//int main(){
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    while(true){
+//        string tmp;
+//        getline(cin,tmp);
+//        if (tmp.compare("EOI")==0){
+//            break;
+//        }
+//        bool ok=false;
+//        for (int i = 0 ; i < tmp.size()-4+1;i++){
+//            if ((tmp[i]=='n'||tmp[i]=='N')&&(tmp[i+1]=='e'||tmp[i+1]=='E')&&(tmp[i+2]=='m'||tmp[i+2]=='M')&&(tmp[i+3]=='o'||tmp[i+3]=='O')){
+//                cout<<"Found"<<'\n';
+//                ok=true;
+//                break;
+//            }
+//        }
+//        if (!ok){
+//            cout<<"Missing"<<'\n';
+//        }
+//    }
+//}
---
+++ boj2011.cpp
@@ -0,0 +1,45 @@
+//
+//  boj2011.cpp
+//  cplusplus
+//
+//  Created by 강명진 on 2022/02/27.
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int n;
+string Code;
+bool zero=true;
+int cache[5001];
+
+int go(int index){
+    if (index==n)return 1;
+    int& ret=cache[index];
+    if (ret!=-1)return ret;
+    ret=0;
+    char now=Code[index];
+    int next=Code[index+1]-'0';
+    bool ok=true;
+    if (index<n&&((now=='2'&&next<=6)||now=='1')){
+        ret+=go(index+2)%1000000;
+        ok=false;
+    }
+    if (ok&&now=='0'){
+        return 0;
+    }
+    return ret+=go(index+1)%1000000;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>Code;
+    n=Code.size();
+    memset(cache,-1,sizeof(cache));
+    int answer=go(0)%1000000;
+    cout<<answer<<'\n';
+    
+}
