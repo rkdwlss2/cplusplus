dbc108275ea41c5ac6dcf425d1d8bd9056e9e85e
rkdwlss2 <rkdwlss2@gmail.com>
2022-03-01 22:49:08 +0900

    boj17141

--- boj15591.cpp
+++ boj15591.cpp
@@ -1,65 +1,65 @@
+////
+////  boj15591.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/02/28.
+////
 //
-//  boj15591.cpp
-//  cplusplus
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//using namespace std;
+//int n,q;
+//vector<pair<int,int>> graph[5001];
+//bool visited[5001];
+//int answer[5001][5001];
 //
-//  Created by 강명진 on 2022/02/28.
+//void bfs(int sNum){ //bfs를 사용하는 이유는 항상 mootube가 트리 구조라 생각함 그리고 한 정점에서 다른 정점으로 갈때
+//    fill(visited,visited+5001,false); // 방문 배열 초기화
+//    queue<pair<int,int>> q;
+//    q.push({sNum,1000000001}); //처음에는 가장 큰값을 넣음
+//    visited[sNum]=true;
+//    while(!q.empty()){ //bfs를 돌림..
+//        auto now = q.front();q.pop();
+//        int num=now.first;
+//        int cost=now.second;
+//        for (auto next : graph[num]){
+//            int nNum=next.first;
+//            int nCost=next.second;
+//            if (visited[nNum])continue;
+//            visited[nNum]=true;
+//            int costMin=min(cost,nCost); // min함수로 USADO의 최솟값 찾음
+//            answer[sNum][nNum]=costMin; //answer 배열에 저장
+//            q.push({nNum,costMin}); //그 다음을 q에 넣음
+//        }
+//    }
+//}
 //
-
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-using namespace std;
-int n,q;
-vector<pair<int,int>> graph[5001];
-bool visited[5001];
-int answer[5001][5001];
-
-void bfs(int sNum){ //bfs를 사용하는 이유는 항상 mootube가 트리 구조라 생각함 그리고 한 정점에서 다른 정점으로 갈때
-    fill(visited,visited+5001,false); // 방문 배열 초기화
-    queue<pair<int,int>> q;
-    q.push({sNum,1000000001}); //처음에는 가장 큰값을 넣음
-    visited[sNum]=true;
-    while(!q.empty()){ //bfs를 돌림..
-        auto now = q.front();q.pop();
-        int num=now.first;
-        int cost=now.second;
-        for (auto next : graph[num]){
-            int nNum=next.first;
-            int nCost=next.second;
-            if (visited[nNum])continue;
-            visited[nNum]=true;
-            int costMin=min(cost,nCost); // min함수로 USADO의 최솟값 찾음
-            answer[sNum][nNum]=costMin; //answer 배열에 저장
-            q.push({nNum,costMin}); //그 다음을 q에 넣음
-        }
-    }
-}
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n>>q;
-    for (int i = 0 ; i <n-1;i++){
-        int a,b,c;
-        cin>>a>>b>>c;
-        graph[a].push_back({b,c});
-        graph[b].push_back({a,c});
-    }
-    
-    for (int i =1;i<=n;i++){
-        bfs(i);
-    }
-    
-    for (int i=0;i<q;i++){
-        int k,v;
-        cin>>k>>v;
-        int ans=0;
-        for (int i = 1;i<=n;i++){ //v지점과 모든 지점을 찾아가며 cost보다 같거나 큰 경우이면 count늘려준다.
-            int cost=answer[v][i];
-            if (cost>=k)ans++;
-        }
-        cout<<ans<<'\n';
-    }
-    
-}
+//int main(){
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    cin>>n>>q;
+//    for (int i = 0 ; i <n-1;i++){
+//        int a,b,c;
+//        cin>>a>>b>>c;
+//        graph[a].push_back({b,c});
+//        graph[b].push_back({a,c});
+//    }
+//    
+//    for (int i =1;i<=n;i++){
+//        bfs(i);
+//    }
+//    
+//    for (int i=0;i<q;i++){
+//        int k,v;
+//        cin>>k>>v;
+//        int ans=0;
+//        for (int i = 1;i<=n;i++){ //v지점과 모든 지점을 찾아가며 cost보다 같거나 큰 경우이면 count늘려준다.
+//            int cost=answer[v][i];
+//            if (cost>=k)ans++;
+//        }
+//        cout<<ans<<'\n';
+//    }
+//    
+//}
---
+++ boj17141 boj17141.cpp
@@ -0,0 +1,100 @@
+//
+//  boj17141 boj17141.cpp
+//  cplusplus
+//
+//  Created by 강명진 on 2022/03/01.
+//
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+int n,k,ANSWER=9999999;
+int board[51][51];
+int dx[4]={0,0,1,-1};
+int dy[4]={1,-1,0,0};
+vector<pair<int,int>> q;
+queue<pair<int,int>> que;
+vector<pair<int,int>> viruses;
+vector<int> v;
+enum State{
+    EMPTY,WALL,VIRUS
+};
+int visited[51][51];
+void bfs(){
+    while(!que.empty()){
+        auto now = que.front();que.pop();
+        int x=now.first;
+        int y=now.second;
+        for (int dir=0;dir<4;dir++){
+            int nx=x+dx[dir];
+            int ny=y+dy[dir];
+            if (nx<0||nx>=n||ny<0||ny>=n)continue;
+            if (board[nx][ny]==WALL||visited[nx][ny]<=visited[x][y]+1)continue;
+            visited[nx][ny]=visited[x][y]+1;
+            que.push({nx,ny});
+        }
+    }
+}
+int Check(){
+    int MAX=-1;
+    for (int i = 0 ; i < n;i++){
+        for (int j = 0 ; j <n;j++){
+            if (visited[i][j]==999999&&board[i][j]==EMPTY)return false;
+            if (board[i][j]!=WALL&&MAX<visited[i][j])MAX=visited[i][j];
+        }
+    }
+    return MAX;
+}
+
+void dfs(int index,int num){
+    if (num==k){
+        for (int i = 0 ; i <n;i++){
+            fill(visited[i],visited[i]+n,999999);
+        }
+        for (auto i  : q){
+            visited[i.first][i.second]=1;
+            que.push(i);
+        }
+        bfs();
+//        for (int i = 0 ; i <n;i++){
+//            for (int j = 0 ; j <n;j++){
+//                cout<<visited[i][j]<<' ';
+//            }cout<<'\n';
+//        }
+        int ans=Check();
+        if (!ans)return;
+        if (ANSWER>ans)ANSWER=ans;
+        return;
+    }
+    for (int i = index+1 ; i <viruses.size();i++){
+        int x=viruses[i].first;
+        int y=viruses[i].second;
+        q.push_back({viruses[i].first,viruses[i].second});
+        dfs(i,num+1);
+        q.pop_back();
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>k;
+    
+    
+    for (int i = 0 ; i <n;i++){
+        for (int j = 0 ; j <n;j++){
+            cin>>board[i][j];
+            if (board[i][j]==VIRUS){
+                viruses.push_back({i,j});
+            }
+        }
+    }
+    dfs(-1,0);
+    if (ANSWER==9999999){
+        cout<<-1<<'\n';
+        exit(0);
+    }
+    
+    cout<<ANSWER-1<<'\n';
+}
