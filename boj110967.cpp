f316d08d811bfedd1948f7cab245c4da66d442e6
rkdwlss2 <rkdwlss2@gmail.com>
2022-03-09 21:10:23 +0900

    boj2302

--- boj110967.cpp
+++ boj110967.cpp
@@ -1,97 +1,97 @@
+////
+////  boj110967.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/03/08.
+////
 //
-//  boj110967.cpp
-//  cplusplus
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/03/08.
+//int n,m;
 //
-
-#include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
-
-int n,m;
-
-vector<pair<int,int>> graph[101][101];
-
-bool blink[101][101];
-
-bool visited[101][101];
-
-int dx[4]={0,0,1,-1};
-int dy[4]={1,-1,0,0};
-
-void dfs(int x,int y){
-    for (auto i : graph[x][y]){
-        int nx=i.first;
-        int ny=i.second;
-        blink[nx][ny]=true;
-        for (int dir =0 ; dir<4;dir++){
-            int nnx=nx+dx[dir];
-            int nny=ny+dy[dir];
-            if (nnx<0||nnx>=n||nny<0||nny>=n)continue;
-            if (visited[nnx][nny]==true&&blink[nx][ny]==true&&visited[nx][ny]==false){
-                visited[nx][ny]=true;
-                dfs(nx,ny);
-            }
-        }
-    }
-    for (int dir = 0 ; dir<4;dir++){
-        int nx=x+dx[dir];
-        int ny=y+dy[dir];
-        if (nx<0||nx>=n||ny<0||ny>=n)continue;
-        if (blink[nx][ny]==false||visited[nx][ny]==true)continue;
-        visited[nx][ny]=true;
-        dfs(nx,ny);
-    }
-    
-    
-}
-
-
-//void bfs(){
-//    queue<pair<int,int>> q;
-//    visited[0][0]=true;
-//    for (auto i : graph[0][0]){
-//        blink[i.first][i.second]=true;
-//    }
-//    q.push({0,0});
-//    while(!q.empty()){
-//        auto now = q.front();q.pop();
-//        int x=now.first;
-//        int y=now.second;
-//        for (auto i : graph[x][y]){
-//            blink[i.first][i.second]=true;
-//        }
-//        for (int dir = 0 ; dir<4;dir++){
-//            int nx=x+dx[dir];
-//            int ny=y+dy[dir];
-//            if (nx<0||nx>=n||ny<0||ny>=n)continue;
-//            if (blink[nx][ny]==false||visited[nx][ny]==true)continue;
-//            visited[nx][ny]=true;
-//            q.push({nx,ny});
+//vector<pair<int,int>> graph[101][101];
+//
+//bool blink[101][101];
+//
+//bool visited[101][101];
+//
+//int dx[4]={0,0,1,-1};
+//int dy[4]={1,-1,0,0};
+//
+//void dfs(int x,int y){
+//    for (auto i : graph[x][y]){
+//        int nx=i.first;
+//        int ny=i.second;
+//        blink[nx][ny]=true;
+//        for (int dir =0 ; dir<4;dir++){
+//            int nnx=nx+dx[dir];
+//            int nny=ny+dy[dir];
+//            if (nnx<0||nnx>=n||nny<0||nny>=n)continue;
+//            if (visited[nnx][nny]==true&&blink[nx][ny]==true&&visited[nx][ny]==false){
+//                visited[nx][ny]=true;
+//                dfs(nx,ny);
+//            }
 //        }
 //    }
+//    for (int dir = 0 ; dir<4;dir++){
+//        int nx=x+dx[dir];
+//        int ny=y+dy[dir];
+//        if (nx<0||nx>=n||ny<0||ny>=n)continue;
+//        if (blink[nx][ny]==false||visited[nx][ny]==true)continue;
+//        visited[nx][ny]=true;
+//        dfs(nx,ny);
+//    }
+//    
+//    
 //}
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n>>m;
-    for (int i = 0 ; i <m;i++){
-        int a,b,c,d;
-        cin>>a>>b>>c>>d;
-        graph[a-1][b-1].push_back({c-1,d-1});
-    }
-    visited[0][0]=true;
-    blink[0][0]=true;
-   // bfs();
-    dfs(0,0);
-    int Sum=0;
-    for (int i = 0 ; i < n;i++){
-        for (int j = 0 ; j <n;j++){
-            if (blink[i][j])Sum++;
-        }
-    }
-    cout<<Sum<<'\n';
-}
+//
+//
+////void bfs(){
+////    queue<pair<int,int>> q;
+////    visited[0][0]=true;
+////    for (auto i : graph[0][0]){
+////        blink[i.first][i.second]=true;
+////    }
+////    q.push({0,0});
+////    while(!q.empty()){
+////        auto now = q.front();q.pop();
+////        int x=now.first;
+////        int y=now.second;
+////        for (auto i : graph[x][y]){
+////            blink[i.first][i.second]=true;
+////        }
+////        for (int dir = 0 ; dir<4;dir++){
+////            int nx=x+dx[dir];
+////            int ny=y+dy[dir];
+////            if (nx<0||nx>=n||ny<0||ny>=n)continue;
+////            if (blink[nx][ny]==false||visited[nx][ny]==true)continue;
+////            visited[nx][ny]=true;
+////            q.push({nx,ny});
+////        }
+////    }
+////}
+//
+//int main(){
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    cin>>n>>m;
+//    for (int i = 0 ; i <m;i++){
+//        int a,b,c,d;
+//        cin>>a>>b>>c>>d;
+//        graph[a-1][b-1].push_back({c-1,d-1});
+//    }
+//    visited[0][0]=true;
+//    blink[0][0]=true;
+//   // bfs();
+//    dfs(0,0);
+//    int Sum=0;
+//    for (int i = 0 ; i < n;i++){
+//        for (int j = 0 ; j <n;j++){
+//            if (blink[i][j])Sum++;
+//        }
+//    }
+//    cout<<Sum<<'\n';
+//}
---
+++ boj2302.cpp
@@ -0,0 +1,45 @@
+//
+//  boj2302.cpp
+//  cplusplus
+//
+//  Created by 강명진 on 2022/03/09.
+//
+
+#include <iostream>
+
+using namespace std;
+
+#define lli long long int
+int n,m;
+bool vip[41];
+lli cache[41];
+lli dp(int num){
+    if (num==n){
+        return 1;
+    }
+    lli& ret= cache[num];
+    if (ret!=-1)return ret;
+    ret = 0;
+    if (vip[num]){
+        ret+=dp(num+1);
+        return ret;
+    }
+    ret+=dp(num+1);
+    if ((num+1<n)&&!vip[num+1]){
+        ret+=dp(num+2);
+    }
+    return ret;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>m;
+    for (int i = 0 ; i <m;i++){
+        int tmp;
+        cin>>tmp;
+        vip[tmp-1]=true;
+    }
+    fill(cache,cache+41,-1);
+    cout<<dp(0)<<'\n';
+}
