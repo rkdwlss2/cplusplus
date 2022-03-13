78e4b58733eb46517e6baa4ad0803f843a5bedb3
rkdwlss2 <rkdwlss2@gmail.com>
2022-03-13 22:19:28 +0900

    boj14645

---
+++ boj14645.cpp
@@ -0,0 +1,21 @@
+//
+//  boj14645.cpp
+//  cplusplus
+//
+//  Created by 강명진 on 2022/03/13.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,k;
+    cin>>n>>k;
+    for (int i = 0 ; i < n ;i++){
+        int a,b;
+        cin>>a>>b;
+    }
+    cout<<"비와이"<<'\n';
+}
