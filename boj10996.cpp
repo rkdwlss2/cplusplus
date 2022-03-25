ee2da73b9a8922e3c9b6cb0c25f927d03a00437f
rkdwlss2 <rkdwlss2@gmail.com>
2022-03-25 22:13:30 +0900

    boj16916

--- boj10996.cpp
+++ boj10996.cpp
@@ -1,24 +1,24 @@
+////
+////  boj10996.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/03/24.
+////
 //
-//  boj10996.cpp
-//  cplusplus
+//#include <iostream>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/03/24.
-//
-
-#include <iostream>
-using namespace std;
-
-int main(){
-    int n;
-    cin>>n;
-    for (int i = 0 ; i < 2*n ; i++){
-        for (int j = 0 ; j <n;j++){
-            if ((i+j)%2==0){
-                cout<<'*';
-            }else{
-                cout<<' ';
-            }
-        }
-        cout<<'\n';
-    }
-}
+//int main(){
+//    int n;
+//    cin>>n;
+//    for (int i = 0 ; i < 2*n ; i++){
+//        for (int j = 0 ; j <n;j++){
+//            if ((i+j)%2==0){
+//                cout<<'*';
+//            }else{
+//                cout<<' ';
+//            }
+//        }
+//        cout<<'\n';
+//    }
+//}
