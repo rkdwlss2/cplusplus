4fc5ec5389aa484423b1467f1ccc14cde9113328
rkdwlss2 <rkdwlss2@gmail.com>
2022-04-10 19:06:58 +0900

    코딜리티 예시문제

--- boj4470.cpp
+++ boj4470.cpp
@@ -1,24 +1,24 @@
+////
+////  boj4470.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/04/07.
+////
 //
-//  boj4470.cpp
-//  cplusplus
+//#include <iostream>
+//#include <string>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/04/07.
-//
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n;
-    cin>>n;
-    string str;
-    getline(cin,str);
-    for (int i = 0 ; i <n;i++){
-        
-        getline(cin,str);
-        cout<<i+1<<". "<<str<<'\n';
-    }
-}
+//int main(){
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    int n;
+//    cin>>n;
+//    string str;
+//    getline(cin,str);
+//    for (int i = 0 ; i <n;i++){
+//        
+//        getline(cin,str);
+//        cout<<i+1<<". "<<str<<'\n';
+//    }
+//}
