7ddee5da9331332f7044dffcf8b06b22ea5e78a0
rkdwlss2 <rkdwlss2@gmail.com>
2022-04-26 23:13:21 +0900

    boj1547

--- boj5176-2.cpp
+++ boj5176-2.cpp
@@ -1,31 +1,31 @@
+////
+////  boj5176-2.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/04/18.
+////
 //
-//  boj5176-2.cpp
-//  cplusplus
+//#include <iostream>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/04/18.
+//int arr[501];
 //
-
-#include <iostream>
-using namespace std;
-
-int arr[501];
-
-int main(){
-    int tc;
-    cin>>tc;
-    while(tc--){
-        fill(arr,arr+501,0);
-        int a,b;
-        cin>>a>>b;
-        for (int i = 0 ; i <a;i++){
-            int tmp;
-            cin>>tmp;
-            arr[tmp]++;
-        }
-        int answer=0;
-        for (int i=1;i<=b;i++){
-            if (arr[i]>0)answer++;
-        }
-        cout<<a-answer<<'\n';
-    }
-}
+//int main(){
+//    int tc;
+//    cin>>tc;
+//    while(tc--){
+//        fill(arr,arr+501,0);
+//        int a,b;
+//        cin>>a>>b;
+//        for (int i = 0 ; i <a;i++){
+//            int tmp;
+//            cin>>tmp;
+//            arr[tmp]++;
+//        }
+//        int answer=0;
+//        for (int i=1;i<=b;i++){
+//            if (arr[i]>0)answer++;
+//        }
+//        cout<<a-answer<<'\n';
+//    }
+//}
