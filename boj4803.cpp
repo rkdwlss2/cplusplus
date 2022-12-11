3825d3aa9c96962e614ba0c1a44bc0cceb7c2809
rkdwlss2 <rkdwlss2@gmail.com>
2022-12-11 14:52:33 +0900

    문자열 다루기

--- boj4803.cpp
+++ boj4803.cpp
@@ -1,51 +1,51 @@
+////
+////  boj4803.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2022/11/20.
+////
 //
-//  boj4803.cpp
-//  cplusplus
+//#include <iostream>
+//#include <vector>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/11/20.
+//vector<vector<int>> v;
+//int visited[501];
 //
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<vector<int>> v;
-int visited[501];
-
-int dfs(int x,int prev){
-    int ret = 1;
-    if (visited[x]==1){
-        return 0;
-    }
-    visited[x]=1;
-    for (int i = 0 ; i<v[x].size();i++){
-        int next = v[x][i];
-        if (prev!=next){
-            ret += dfs(next,x);
-        }
-    }
-    return ret;
-}
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    while(true){
-        int n,m;
-        cin>>n>>m;
-        for (int i = 0 ; i <m;i++){
-            int a,b;
-            cin>>a>>b;
-            v[a].push_back(b);
-            v[b].push_back(a);
-        }
-        int cnt = 0;
-        for (int i = 1 ; i <= n;i++){
-            if (visited[i]!=1){
-                if (dfs(i,-1)>0) cnt++;
-            }
-        }
-    }
-    
-    
-}
+//int dfs(int x,int prev){
+//    int ret = 1;
+//    if (visited[x]==1){
+//        return 0;
+//    }
+//    visited[x]=1;
+//    for (int i = 0 ; i<v[x].size();i++){
+//        int next = v[x][i];
+//        if (prev!=next){
+//            ret += dfs(next,x);
+//        }
+//    }
+//    return ret;
+//}
+//
+//int main(){
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    while(true){
+//        int n,m;
+//        cin>>n>>m;
+//        for (int i = 0 ; i <m;i++){
+//            int a,b;
+//            cin>>a>>b;
+//            v[a].push_back(b);
+//            v[b].push_back(a);
+//        }
+//        int cnt = 0;
+//        for (int i = 1 ; i <= n;i++){
+//            if (visited[i]!=1){
+//                if (dfs(i,-1)>0) cnt++;
+//            }
+//        }
+//    }
+//
+//
+//}
