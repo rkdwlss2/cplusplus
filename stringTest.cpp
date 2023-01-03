750f4ba7eeb7fdea5f62c4f8fa8703ab26cf8ca9
rkdwlss2 <rkdwlss2@gmail.com>
2023-01-03 22:27:18 +0900

    c++ string 지식

---
+++ stringTest.cpp
@@ -0,0 +1,29 @@
+////
+////  stringTest.cpp
+////  cplusplus
+////
+////  Created by 강명진 on 2023/01/03.
+////
+//
+//#include <string>
+//#include <iostream>
+//using namespace std;
+//
+//int main(){
+//    string a = "it is time to study",res;
+//    int pos, max = INT_MIN;
+//    while ((pos=a.find(' '))!=string::npos){
+//        string tmp = a.substr(0,pos);
+//        int len = tmp.size();
+//        if (len > max){
+//            max = len;
+//            res = tmp;
+//        }
+////        cout<<tmp<<'\n';
+//        a = a.substr(pos+1);
+//    }
+//    if (a.size()>max){
+//        res = a;
+//    }
+//    cout<<res<<'\n';
+//}
